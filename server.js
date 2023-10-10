d67b2a21d83985600e49c2e4923ade9a39c8fe81
Yurii Novikov <urichcoolx@gmail.com>
2023-10-11 01:00:38 +0200

    created-home-router

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const expess = require("express");
+const morgan = require("morgan");
 const app = expess();
 
+const {homeRouter} = require('./homeRouter')
+
 const PORT = 8081;
+
+app.use(expess.json());
+app.use(expess.urlencoded({ extended: true }));
+app.use(expess.static('public'));
+app.use(morgan('tiny'));
+app.use(homeRouter)
 
 app.get("/home", (req, res) => {
   res.sendStatus(200);
