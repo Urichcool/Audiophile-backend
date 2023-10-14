52daec4bd049d3c7e266f26b183897f960c7608d
Yurii Novikov <urichcoolx@gmail.com>
2023-10-14 03:17:19 +0200

    created-database

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const expess = require("express");
 const morgan = require("morgan");
+const MongoClient = require("mongodb").MongoClient;
+const { connectMongo } = require("./db/conection");
 const app = expess();
+const cors = require("cors");
+require("dotenv").config();
 
 const {homeRouter} = require('./homeRouter')
-
-const PORT = 8081;
+const PORT = process.env.PORT || 3001;
 
 app.use(expess.json());
 app.use(expess.urlencoded({ extended: true }));
 app.use(expess.static('public'));
 app.use(morgan('tiny'));
 app.use(homeRouter)
+app.use(cors());
 
 app.get("/home", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(PORT, (err) => {
-  if (err) console.error("Error at server launch", err);
-  console.log(`Server works at port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectMongo();
+    console.log("Database connection successful");
+    app.listen(PORT, (err) => {
+      if (err) console.error("Error at aserver launch", err);
+      console.log(`Server works at port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+start();
