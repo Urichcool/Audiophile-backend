8e986b182b972d7080c44c71cea49083f0925be8
Yurii Novikov <urichcoolx@gmail.com>
2024-05-06 23:52:36 +0200

    created-order-route

---
+++ controllers/order.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+
+const postNewOrder = async (
+    req: Request,
+    res: Response
+): Promise<void | Response<any, Record<string, any>>> => { 
+    
+}
